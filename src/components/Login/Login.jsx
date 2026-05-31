09628f93a0aca74f4dd6390a1f4acba6bdc36fec
Gabriel Wendling <gabrielwendling342@gmail.com>
2026-05-30 21:37:37 -0300

    adicionei background,logo e texto amanha eu tento consertar e remover os textes pra deicar mais parecido com a imagem

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,26 +1,70 @@
+import { useState, useEffect } from "react";
 import styles from "./Login.module.css";
+import background from "../../assets/overwatchbackground.jpeg";
+import logo from "../../assets/overwatch2logovsign.png";
+import logoText from "../../assets/Overwatch2logotext.png";
 
 function Login() {
-  return (
-    <div className={styles.container}>
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
 
-      <img alt="Overwatch Logo" />
+  const loginValido = email === "admin" && senha === "123";
+
+const mostrarErro =
+  email !== "" &&
+  senha !== "" &&
+  !loginValido;
+
+  useEffect(() => {
+    console.log("Email ou senha alterados");
+  }, [email, senha]);
+
+  return (
+    <div
+  className={styles.container}
+  style={{
+    backgroundImage: `url(${background})`
+  }}
+>
+      <img   
+      src={logo}
+      alt="Overwatch 2 logo"
+      className={styles.logo}
+      />
+
+      <img
+     src={logoText}
+     alt="Overwatch Text"
+     className={styles.logoText}
+     
+     />
 
       <input
-      className={styles.input}
+        className={styles.input}
         type="text"
         placeholder="Email or Phone"
+        value={email}
+        onChange={(event) => setEmail(event.target.value)}
       />
 
       <input
-      className={styles.input}
+        className={styles.input}
         type="password"
         placeholder="Password"
+        value={senha}
+        onChange={(event) => setSenha(event.target.value)}
       />
 
-      <button className={styles.input}>
-        LOGIN
-      </button>
+      <p>Login válido: {loginValido ? "Sim" : "Não"}</p>
+
+{mostrarErro && (
+  <p>Email ou senha incorretos. Verifique seus dados e tente novamente.</p>
+)}
+
+
+<button className={styles.button}>
+  {loginValido ? "ENTRAR" : "LOGIN"}
+</button>
     </div>
   );
 }
